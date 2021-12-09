698654d41c19a37edcb7d33b5c1884adaca67ecc
WAHEEDIANHO <waheedianho65@gmail.com>
2021-12-09 04:54:07 +0100

    Completed module

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,3 +1,20 @@
+
+// // import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-product-page',
+//   templateUrl: './product-page.component.html',
+//   styleUrls: ['./product-page.component.css']
+// })
+// export class ProductPageComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
